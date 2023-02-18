91eb7391cb64f36fbbd8c2ba5912b9921f8791d3
Cdawgy <cdawgy@cdawgys-mbp.home>
2023-02-18 18:29:13 +0000

    Added general function to upload and retrieve sound files from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import React from "react";
+import { AudioRecorder } from "react-audio-voice-recorder";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
+import { EndPoints } from "./constants/PostRequests";
+import DisplaySounds from "./components/DisplaySounds";
 
 function App() {
+  let audioUrl: Blob | undefined = undefined;
+  const addAudioElement = (blob: Blob) => {
+    audioUrl = blob;
+  };
+
+  const uploadSoundClip = () => {
+    if (audioUrl == undefined) {
+      alert("No audio defined");
+      return;
+    }
+    axios
+      .post(
+        EndPoints.saveSoundToDb,
+        {
+          file: audioUrl,
+        },
+        { headers: { "Content-Type": "multipart/form-data" } }
+      )
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Converted to <code>Typescript</code>
-        </p>
+      <header className="App-header" id="header">
+        <p>Sound Recording Demo</p>
+        <AudioRecorder
+          onRecordingComplete={(blob: Blob) => addAudioElement(blob)}
+        ></AudioRecorder>
+        <button onClick={uploadSoundClip}>Upload Sound Recording</button>
+        <DisplaySounds />
       </header>
     </div>
   );
---
+++ src/constants/PostRequests.tsx
@@ -0,0 +1,6 @@
+export const EndPoints = {
+  saveSoundToDb:
+    "https://prod-23.ukwest.logic.azure.com:443/workflows/93d744bdbebc4423b2639f45214bf3c2/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=BIcSdjfLnu-M0tR2FvX0MxmAVbZA7GEmYvr2gEG_FEA",
+  getAllSoundClips:
+    "https://prod-13.ukwest.logic.azure.com:443/workflows/42b334bc1a5c405fa38e0cd87220c2dc/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=fZlVyd58n33dHhmCnLZ4pG-ZKxWDi2XgblYuM-nGKBw",
+};
